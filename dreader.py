cc8a7318bce1e1a0da81540c2e41cf7def974393
joshua.strang3@gmail.com <joshua.strang3@gmail.com@d8934f9d-720b-635e-2af8-c96fa37779b7>
2010-03-22 23:16:13 +0000

    dreader done

--- dreader.py
+++ dreader.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python
 '''
-package dreader
 Reading in Dicionary Data
 ./myprog <path_to_questions.txt> <path_to_termfreq.dat> <path_to_topics.txt> 
 <path_to_test_questions.txt> [<path_to_dictionary.txt>]
 '''
-
 import sys,os
 
-#def parse_dict(path):
-fh = open("dictionary.txt")
-for line in fh.readlines():
-  print line
+def parse_dict(path):
+  fh = open(path,'r')
+  dict = {}
+  for line in fh.readlines():
+   tmp = line.strip().split()
+   dict[tmp[1]] = tmp[0]
+   #dict[tmp[0]] = tmp[1]
+
+
+  return dict
+
+id = parse_dict("dictionary.txt")
+print id
