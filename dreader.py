9e8c4e48caa0d56ba8ca950d85df2b408dd1666e
joshua.strang3@gmail.com <joshua.strang3@gmail.com@d8934f9d-720b-635e-2af8-c96fa37779b7>
2010-03-22 21:37:51 +0000

    the text files and part of my parser

---
+++ dreader.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+'''
+package dreader
+Reading in Dicionary Data
+./myprog <path_to_questions.txt> <path_to_termfreq.dat> <path_to_topics.txt> 
+<path_to_test_questions.txt> [<path_to_dictionary.txt>]
+'''
+
+import sys,os
+
+#def parse_dict(path):
+fh = open("dictionary.txt")
+for line in fh.readlines():
+  print line
