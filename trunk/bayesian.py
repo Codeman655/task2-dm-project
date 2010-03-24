49f6e0c1795903464ae08dd9d654683c700bacca
sethdelliott <sethdelliott@d8934f9d-720b-635e-2af8-c96fa37779b7>
2010-03-24 22:41:21 +0000

    fixin' more stuff

--- trunk/bayesian.py
+++ trunk/bayesian.py
@@ -40,8 +40,8 @@ term_dict = parse_term(termfreq_path)
 # print "572 keys:", term_dict[572].keys()
 # print "796 hits:", term_dict[572][790]
 
-parse_dict(dict_path)
+print parse_dict(dict_path)
 
 matrix = create_matrix(questOD, term_dict)
 
-
+# print parse_topics(topics_path)
