deb3cce9973981c60180004b7bb770dbd3ae07a4
Codeman655 <Codeman655@d8934f9d-720b-635e-2af8-c96fa37779b7>
2010-03-25 20:03:44 +0000

    working on formulas

--- formulas.py
+++ formulas.py
@@ -9,8 +9,31 @@ import numpy
 
 #Probability generator
 #Cond probablitiy of term in the topic
-def probability(matrix, topics, question){
-  for term in question:
-}
+def probability(quest, qNum, freqMatrix, topicProb)
+  #Use the term frequency matrix to generate a probability/topic dictionary
+  print topicProb
+  
+  for term in quest:
+    tProb=(frequency of terms in the topic)
+
+    
+    
+    
+  
+    
+
+
+
+
+#Calculate probability of topic per tital list of questions
+def topicProb(topicDict):
+  topic_calc = {}
+  for i in range(1,42):
+    count = 0
+    for topic in topicDict:
+      if topic[i] == i
+        count += 1
+    topic_calc{i} = double(count/41)
+  return topic_calc
 
 #Cond probablitiy of the 
