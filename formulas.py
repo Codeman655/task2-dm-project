b967f8da0ea28c32246e874af339f9f3e4c18701
Codeman655 <Codeman655@d8934f9d-720b-635e-2af8-c96fa37779b7>
2010-03-26 20:59:34 +0000

    probabilities are up. BITCHES. And we're meeting tomorrow, I'll send an email. Peace

--- formulas.py
+++ formulas.py
@@ -22,7 +22,7 @@ def probability(quest, qNum, freqMatrix, tProb, topicDict, termDict):
 
   tProbList = topicProb(topicDict)
 
-  factor=[]
+  factor=[] 
   for term in quest:
     #Goal=(frequency of terms in the topic)
     tIndex = get_word_index(term, termDict)
@@ -30,30 +30,45 @@ def probability(quest, qNum, freqMatrix, tProb, topicDict, termDict):
       continue
     else:
       fij = freqMatrix[qNum][tIndex-1] #frequency of the term in the question
-      print fij
-'''
+
     #This needs to be in a seperate function
     qTopic = topicDict[qNum] #Grabbin' the topic
+    print qTopic
     tmpList = []
     term_subtotal = 0
     for tup in topicDict.iteritems():
       if tup[1] == qTopic: #if the value == the topic of the question
+        print tup
         tmpList.append(tup[0]);
     for tmp in tmpList:
-      term_subtotal += freqMatrix[tmp][tIndex] #Sum the frequencies
+      term_subtotal += freqMatrix[tmp][tIndex-1] #Sum the frequencies
+    print term_subtotal
 
-    term_subtotal /= len(freqMatrix[tIndex])  
-    term_subtotal /= tProbList[qNum] #This is P(term & topic) Need to check
-    print "P(Term in Topic) = " + term_subtotal
+    print len(freqMatrix[tIndex-1])
+    if len(freqMatrix[tIndex-1]) == 0:
+      print "error: len = 0, 1"
+      return -1
+    term_subtotal = float(term_subtotal) / len(freqMatrix[tIndex-1])  
+
+    if tProbList[qNum+1] == 0:
+      print "qnum: "
+      print qNum
+      print tProbList
+      print "error: len = 0, 2"
+      return -1
+
+    term_subtotal /= tProbList[qNum+1] #This is P(term & topic) Need to check
+    print "P(Term in Topic):" 
+    print term_subtotal
     factor.append(fij*term_subtotal)
   
   tmpNum = 1
   for item in factor:
     tmpNum *= item
   tmpNum /= len(quest)
-  print "resulting probability: " + tmpNum
+  print "resulting probability: "
+  print tmpNum
   return tmpNum #whatever this is
-'''
 
 #Calculate probability of topic per tital list of questions
 def topicProb(topicDict):
